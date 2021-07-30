86ed27d05113d27caa79d474c0ac9a2cded7f37f
Nir Gilad <nir@vacayz.com>
2021-07-30 19:20:05 +0300

    add axios call to pixabay and present images on ImageList

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,33 @@
 
 import React from 'react';
 import SearchInput from './SearchInput';
+import axios from 'axios';
+import ImageList from './ImageLIst'
 
-
+const PIXABAY_URL = "https://pixabay.com/api/"
+const PIXABAY_API_KEY = "22708682-754444c59019b293b59d26b70"
 class App extends React.Component {
 
 
   constructor(props) {
     super(props)
     this.onSearchSubmit = this.onSearchSubmit.bind(this)
+    this.state = { images: [] }
   }
 
-  onSearchSubmit(entry) {
-    console.log(entry)
+  async onSearchSubmit(entry) {
+    const params = new URLSearchParams();
+    params.append('key', PIXABAY_API_KEY);
+    params.append('q', entry);
+    params.append('image_type', "photo");
+    params.append('pretty', "true");
+
+    const response = await axios.get(PIXABAY_URL, {
+      params: params
+    })
+    this.setState({ images: response.data.hits })
+    console.log(response.data.hits)
+
   }
 
   render() {
@@ -23,6 +38,7 @@ class App extends React.Component {
 
       <div className="ui container" style={{ marginTop: '30px' }}>
         <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
 
     )
